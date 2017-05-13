be3175deef341c07365ba79791a8103b66beb893
Marc Choisy <mchoisy@gmail.com>
2017-05-13 12:30:06 +0700

    Initial commit

---
+++ R/censusVN2009.R
@@ -0,0 +1,7 @@
+#' censusVN2009: Shape files of the communes, districts and provinces of Vietnam as of after 2008 with 2009 census populations sizes
+#'
+#' The censusVN2009 package provides the polygons of the communes, districts and provinces of Vietnam as defined since 2008, together with the populations sizes from the 2009 census as attributes.
+#'
+#' @docType package
+#' @name censusVN2009
+NULL
---
+++ data-raw/data_creation.R
@@ -0,0 +1,141 @@
+library(rgdal)     # for "readOGR"
+library(maptools)  # for "thinnedSpatialPoly"
+library(dplyr)
+
+provinces_r <- readOGR("data-raw/Polygons", "provinces")
+districts_r <- readOGR("data-raw/Polygons", "districts")
+communes_r <- readOGR("data-raw/Polygons", "communes")
+
+communes_r@data$ADDRESS <- NULL
+names(communes_r@data) <- c("object_id", "commune_id", "district_id",
+                            "commune_vn", "commune", "district_vn", "district",
+                            "province_vn", "province", "province_id",
+                            "shape_length", "shape_area")
+communes_r@data <- communes_r@data[, c("object_id", "province_id", "district_id", "commune_id",
+                                       "province", "district", "commune",
+                                       "province_vn", "district_vn", "commune_vn",
+                                       "shape_length", "shape_area")]
+
+districts_r@data$UniqueIden <- NULL
+names(districts_r@data) <- c("object_id", "district_id",
+                            "district_vn", "district",
+                            "province", "level", "type", "province_id",
+                            "shape_length", "shape_area")
+hash <- with(lapply(unique(communes@data[, c("province", "province_vn")]), as.character),
+             setNames(province_vn, province))
+districts_r@data$province_vn <- hash[districts_r@data$province]
+
+districts_r@data <- districts_r@data[, c("object_id", "province_id", "district_id",
+                                         "province", "district",
+                                         "province_vn", "district_vn",
+                                         "shape_length", "shape_area")]
+
+names(provinces_r@data) <- c("object_id", "province_vn", "province", "province_id",
+                             "shape_length", "shape_area")
+provinces_r@data <- provinces_r@data[, c("object_id", "province_id", "province",
+                                         "province_vn", "shape_length", "shape_area")]
+
+pr <- sub("Ba Ria-Vung Tau", "Ba Ria - Vung Tau", provinces_r@data$province)
+provinces_r@data$province <- sub("Thua Thien Hue", "Thua Thien - Hue", pr)
+
+pr <- sub("Ba Ria Vung Tau", "Ba Ria - Vung Tau", districts_r@data$province)
+pr <- sub("Can Tho city", "Can Tho", pr)
+pr <- sub("Da Nang city", "Da Nang", pr)
+pr <- sub("Hai Phong city", "Hai Phong", pr)
+pr <- sub("Ho Chi Minh city", "Ho Chi Minh", pr)
+pr <- sub("Quang BInh", "Quang Binh", pr)
+districts_r@data$province <- sub("Thua Thien Hue", "Thua Thien - Hue", pr)
+
+pr <- sub("Thua Thien Hue", "Thua Thien - Hue", communes_r@data$province)
+pr <- sub("Ninh Bình", "Ninh Binh", pr)
+communes_r@data$province <- sub("Ba Ria Vung Tau", "Ba Ria - Vung Tau", pr)
+
+# Fixing a commune ID:
+communes_r@data[communes_r@data$district_id == 60141, "district_id"] <- 60114
+
+# Fixing districts:
+districts_to_remove <- names(which(table(communes_r$district_id) < 2))
+communes_r <- subset(communes_r, !(district_id %in% districts_to_remove))
+districts_r <- subset(districts_r, !(district_id %in% districts_to_remove))
+
+tomerge <- subset(districts_r, object_id %in% c(357, 704))
+merged <- maptools::unionSpatialPolygons(tomerge, c(357, 357))
+attr(merged@polygons[[1]]@Polygons[[1]]@coords, "dimnames") <- NULL
+sel <- which(sapply(districts_r@polygons, function(x) x@ID) == 357)
+districts_r@polygons[[sel]] <- merged@polygons[[1]]
+districts_r <- subset(districts_r, !(object_id %in% paste(c(385, 617, 704))))
+
+# ------------------------------------------------------------------------------
+
+census <- readOGR("data-raw/Vietnam_pop_commune", "Vietnam_pop_commune")
+census@data$PRO_NAME_E <- sub("Thua Thien Hue", "Thua Thien - Hue", census@data$PRO_NAME_E)
+census@data$ADDRESS <- NULL
+census@data$Shape_Le_1 <- NULL
+names(census@data) <- c("commune_id", "district_id", "commune_vn", "commune",
+                          "district_vn", "district", "province_vn", "province",
+                          "province_id", "population", "area", "shape_length",
+                          "shape_area")
+census@data <- census@data[, c("commune_id", "district_id", "province_id",
+                                   "commune", "district", "province",
+                                   "commune_vn", "district_vn", "province_vn",
+                                   "population", "area", "shape_length", "shape_area")]
+rownames(census@data) <- NULL
+for(i in seq_along(census@polygons))
+  census@polygons[[i]]@ID <- as.character(as.numeric(census@polygons[[i]]@ID) + 1)
+
+sel <- setdiff(census$commune_id, communes_r$commune_id)
+census <- subset(census, !(commune_id %in% sel))
+tmp <- rownames(communes_r@data)
+communes_r <- merge(communes_r, subset(census@data, , c(commune_id, area, population)))
+rownames(communes_r@data) <- tmp
+
+# ------------------------------------------------------------------------------
+
+tmp <- rownames(districts_r@data)
+districts_r <- communes_r@data %>%
+  group_by(district_id) %>%
+  summarise(area = sum(area), population = sum(population)) %>%
+  merge(districts_r, .)
+rownames(districts_r@data) <- tmp
+
+tmp <- rownames(provinces_r@data)
+provinces_r <- districts_r@data %>%
+  group_by(province_id) %>%
+  summarise(area = sum(area), population = sum(population)) %>%
+  merge(provinces_r, .)
+rownames(provinces_r@data) <- tmp
+
+communes_r@data <- communes_r@data[, c("province_id", "district_id", "commune_id",
+                                       "province", "district", "commune",
+                                       "province_vn", "district_vn", "commune_vn",
+                                       "shape_length", "shape_area", "area",
+                                       "population")]
+
+districts_r@data <- districts_r@data[, c("province_id", "district_id",
+                                         "province", "district",
+                                         "province_vn", "district_vn",
+                                         "shape_length", "shape_area",
+                                         "area", "population")]
+
+provinces_r@data <- provinces_r@data[, c("province_id", "province", "province_vn",
+                                         "shape_length", "shape_area", "area",
+                                         "population")]
+
+# ------------------------------------------------------------------------------
+
+# changing the feature IDs:
+communes_r <- spChFIDs(communes_r, as.character(seq_len(length(communes_r))))
+districts_r <- spChFIDs(districts_r, as.character(seq_len(length(districts_r))))
+provinces_r <- spChFIDs(provinces_r, as.character(seq_len(length(provinces_r))))
+
+# ------------------------------------------------------------------------------
+
+# Thining polygons:
+provinces <- thinnedSpatialPoly(provinces_r, .01)
+districts <- thinnedSpatialPoly(districts_r, .0036)
+communes <- thinnedSpatialPoly(communes_r, .00145)
+
+
+# Saving:
+devtools::use_data(provinces, districts, communes, provinces_r, districts_r,
+                   communes_r, overwrite = TRUE)
