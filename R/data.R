c742b3896b7634a709de3840d776a9b62cd9cf68
Lucie Contamin <contamin.lucie@gmail.com>
2018-09-04 18:38:55 +0700

    Create documentation

---
+++ R/data.R
@@ -0,0 +1,65 @@
+#' Polygons of the communes of Vietnam with population size
+#'
+#' Shape files of the communes of Vietnam as of after 2008 (merging of the
+#' provinces of Ha Tay and Ha Noi) with 2009 census populations sizes in low
+#' polygons resolution. All the Vietnamese names are expressed in UNICODE.
+#'
+#' @usage data(communes)
+#'
+#' @format An object of class \code{SpatialPolygonsDataFrame}
+"communes"
+
+#' Polygons of the communes of Vietnam with population size
+#'
+#' Shape files of the communes of Vietnam as of after 2008 (merging of the
+#' provinces of Ha Tay and Ha Noi) with 2009 census populations sizes in high
+#' polygons resolution. All the Vietnamese names are expressed in UNICODE.
+#'
+#' @usage data(communes_r)
+#'
+#' @format An object of class \code{SpatialPolygonsDataFrame}
+"communes_r"
+
+#' Polygons of the districts of Vietnam with population size
+#'
+#' Shape files of the districts of Vietnam as of after 2008 (merging of the
+#' provinces of Ha Tay and Ha Noi) with 2009 census populations sizes in low
+#' polygons resolution. All the Vietnamese names are expressed in UNICODE.
+#'
+#' @usage data(districts)
+#'
+#' @format An object of class \code{SpatialPolygonsDataFrame}
+"districts"
+
+#' Polygons of the districts of Vietnam with population size
+#'
+#' Shape files of the districts of Vietnam as of after 2008 (merging of the
+#' provinces of Ha Tay and Ha Noi) with 2009 census populations sizes in high
+#' polygons resolution. All the Vietnamese names are expressed in UNICODE.
+#'
+#' @usage data(districts_r)
+#'
+#' @format An object of class \code{SpatialPolygonsDataFrame}
+"districts_r"
+
+#' Polygons of the provinces of Vietnam with population size
+#'
+#' Shape files of the provinces of Vietnam as of after 2008 (merging of the
+#' provinces of Ha Tay and Ha Noi) with 2009 census populations sizes in high
+#' polygons resolution. All the Vietnamese names are expressed in UNICODE.
+#'
+#' @usage data(provinces_r)
+#'
+#' @format An object of class \code{SpatialPolygonsDataFrame}
+"provinces_r"
+
+#' Polygons of the provinces of Vietnam with population size
+#'
+#' Shape files of the provinces of Vietnam as of after 2008 (merging of the
+#' provinces of Ha Tay and Ha Noi) with 2009 census populations sizes in low
+#' polygons resolution. All the Vietnamese names are expressed in UNICODE.
+#'
+#' @usage data(provinces)
+#'
+#' @format An object of class \code{SpatialPolygonsDataFrame}
+"provinces"
